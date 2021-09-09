5a76f3ab34f677bc838cc09380f50046fd6278e2
Joshua Peng <jpa95@sfu.ca>
2021-09-09 00:00:54 -0700

    rearrange order of image

--- src/utils/descriptions.js
+++ src/utils/descriptions.js
@@ -2,19 +2,19 @@
 
 const cows = [
   {
-    src: "./headPhoneCow.svg",
+    src: "./monocleCow.svg",
     title: "I am a coder",
     text: "I like to code up and create neat little projects like this website!",
     padDown: { marginTop: "2vw" },
   },
   {
-    src: "./coolCow.svg",
+    src: "./headPhoneCow.svg",
     title: "I am a producer",
     text: "I like music production and producing music in FL Studio",
     style: { marginTop: "1.5vw" },
   },
   {
-    src: "./monocleCow.svg",
+    src: "./coolCow.svg",
     title: "I am a dancer",
     text: "I like to breakdance and occassionally spin on my head",
     padDown: { marginTop: "2vw" },
